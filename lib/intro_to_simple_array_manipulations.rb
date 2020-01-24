11eaaab935df2def9351d626e5f41b83d19b2b06
Isak Kallenbach <kallenbach13@gmail.com>
2020-01-24 17:06:00 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -13,3 +13,24 @@ end
 def pop_with_args(array)
   array.pop(2)
 end
+
+def using_shift(array)
+  array.shift
+end
+
+def shift_with_args(array)
+  array.shift(2)
+end
+
+def using_concat(array1, array2)
+  array1 + array2
+end
+
+def using_insert(array, element)
+end
+
+def using_uniq
+end
+
+def using_flatten
+end
